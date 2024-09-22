5ca278e93fbee7bfb0d50bd5dd037e77c432ee8a
altravolf <altravolf001@gmail.com>
2024-09-22 22:53:07 +0530

    user setup done

--- elib-apis/src/user/userController.ts
+++ elib-apis/src/user/userController.ts
@@ -26,7 +26,8 @@ const createUser = asyncHandler(async (req: Request, res: Response, next: NextFu
             return next(error);
         }
     } catch (err) {
-        return next(createHttpError(500, "Error while looking for user's pre-existance in Database."))
+        const error = createHttpError(500, "Error while looking for user's pre-existance in Database.");
+        return next(error);
     }
 
 
@@ -51,9 +52,51 @@ const createUser = asyncHandler(async (req: Request, res: Response, next: NextFu
     }
 
     // Sending response
-    res.json({ message: `User Created Successfully. Id: ${newUser._id} Token: ${token}` })
+    // ? Status code 201 when something is created for example user is createds
+    res.status(201).json({ message: `User Created Successfully. Id: ${newUser._id} Token: ${token}` })
 
 })
 
 
-export { createUser };
+const loginUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+
+    // Check all fields are provided by user
+    if (!email || !password) {
+        const error = createHttpError(400, "All field are required!");
+        // If any of the field is missing then send error 
+        return next(error);
+    }
+
+    // ! Code is working without try and catch but with try catch it shows error.
+    // Search for user in database with provided email
+    const existingUser = await user.findOne({ email });
+    // If user is not found then send error
+    if (!existingUser) {
+        return next(createHttpError(404, "Your Account is not found!"));
+    }
+
+    // Compare provided password with hashed password in database
+    const isMatch = await bcrypt.compare(password, (existingUser as User).password);
+    // If password does not match then send error
+    if (!isMatch) {
+        return next(createHttpError(400, "Wrong Email or Password!"));
+    }
+
+    // Generate JWT token for user
+    let jwtToken: string;
+    try {
+        jwtToken = sign({ sub: existingUser._id }, config.jwtToken as string, { expiresIn: "7d" });
+    } catch (err) {
+        // If there is an error while generating token then send error
+        return next(createHttpError(500, "Error while generating Token"));
+    }
+
+    // Send response with JWT token
+    res.json({ message: `User Logged In. ID: ${jwtToken}` })
+
+
+})
+
+
+export { createUser, loginUser };
--- elib-apis/src/user/userModel.ts
+++ elib-apis/src/user/userModel.ts
@@ -11,6 +11,7 @@ const userSchema = new Schema<User>({
     email: {
         type: String,
         required: true,
+        unique: true
     },
     password: {
         type: String,
